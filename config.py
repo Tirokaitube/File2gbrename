0f47cb353f80c3a40307de4d815bec4ef99b078d
Tirokaitube <tirokaitube@gmail.com>
2025-04-25 18:17:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config(object):
 
     # channels logs
     FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002568229104"))
     BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", ""))
 
     # wes response configuration     
