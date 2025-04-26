765e80fdf9f134315c0e08698ecf3976b16f9bb5
Tirokaitube <tirokaitube@gmail.com>
2025-04-26 13:39:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","tirokaitube")     
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://tirokaitube:ymDe3VLWXQ05JazI@cluster0.tktl52k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_URL  = os.environ.get("DB_URL","")
 
     # other configs
     BOT_UPTIME  = time.time()
